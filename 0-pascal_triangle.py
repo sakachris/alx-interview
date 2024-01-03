ece2b407a4b43288a52291454aec9f04c64e35d7
sakachris <sakachris90@gmail.com>
2024-01-03 14:02:27 +0300

    pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' module for pascal triangle '''
+
+
+def pascal_triangle(n):
+    '''
+    making of pascal triangle
+    '''
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
