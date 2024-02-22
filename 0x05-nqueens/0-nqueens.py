2f5ccb20893993e9bdcdde04f7c6f90dfb386cdc
sakachris <sakachris90@mail.com>
2024-02-22 07:59:32 +0300

    nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+''' 0-nqueens.py '''
+
+import sys
+
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen at board[row][col]
+    """
+    # Check this row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, n, solutions):
+    """
+    Utility function to solve N queens problem
+    """
+    # If all queens are placed then return true
+    if col >= n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    # Consider this column and try placing this queen in all rows one by one
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, n, solutions)
+            board[i][col] = 0
+
+
+def solve_nqueens(n):
+    """
+    Solve the N queens problem
+    """
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+
+    solve_nqueens_util(board, 0, n, solutions)
+
+    for sol in solutions:
+        print(sol)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solve_nqueens(N)
