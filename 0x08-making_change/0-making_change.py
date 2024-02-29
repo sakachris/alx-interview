b71b3ee35146cfe09b8b6b7b01493e5c90e6f409
sakachris <sakachris90@mail.com>
2024-02-29 21:26:51 +0300

    make change efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module for making change using dynamic programming
+Module for making change using a greedy algorithm
 """
 
 
@@ -11,11 +11,11 @@ def makeChange(coins, total):
     if total < 1:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    num_coins = 0
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        num_coins += total // coin
+        total %= coin
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return num_coins if total == 0 else -1
